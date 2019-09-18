40877315be8200855012a6a78f462700efa3f582
Moamen Soroor <moamensoroor@gmail.com>
2019-09-18 17:20:38 +0200

    more csharp array training rectangle and jagged array.

--- pro-csharp-book-training/Program.cs
+++ pro-csharp-book-training/Program.cs
@@ -124,12 +124,19 @@ namespace pro_csharp_book_training
             int rows = rect2.GetLength(0);
             int cols = rect2.GetLength(1);
 
+            int rowIndexWidth = rows.ToString().Length;
             int colIndexWidth = cols.ToString().Length;
 
-            Console.WriteLine($"      {}");
+            int rowWidth = "row".Length + rowIndexWidth;
+            Console.Write(Repeate(" ", rowWidth) + "\t");
+            for (int i = 0; i < rect2.GetLength(1); i++)
+            {
+                Console.Write($"col{i}\t");
+            }
+            Console.WriteLine();
             for (int i = 0; i < rect2.GetLength(0); i++)
             {
-                Console.Write($"r[{i}]: ");
+                Console.Write($"row{i}\t");
                 for (int j = 0; j < rect2.GetLength(1); j++)
                 {
                     Console.Write($"{rect2[i, j]}\t");
@@ -138,7 +145,7 @@ namespace pro_csharp_book_training
             }
 
 
-            // Jagged 2D Array
+            // Jagged 2D Array: have different dim lengths
             int[][] arr1 = new int[2][];
 
             int[][] arr2 = new int[2][] { new int [] { 10, 20, 30, 40, 90 }, new int [] { 50, 60, 70, 80 } };
@@ -151,10 +158,104 @@ namespace pro_csharp_book_training
             arr5[0] = new int[] { 10, 20, 30, 40, 90 };
             arr5[1] = new int[] { 101, 201, 301, 401, 901 };
 
+            var arr6 = new int[2][]{ new int []{ 10, 20, 30, 40, 50 }, new int []{ 60,70,80,90} };
+
+            Console.WriteLine("arr6: ");
+            for (int i = 0; i < arr6.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr6[i].GetLength(0); j++)
+                {
+                    Console.Write($"{arr6[i][j]}\t");
+                }
+                Console.WriteLine();
+            }
+
+            var arr7 = new string[2][][];
+            arr7[0] = new string[3][];
+            arr7[1] = new string[4][];
+
+            arr7[0][0] = new string[3];
+            arr7[0][1] = new string[4];
+            arr7[0][2] = new string[5];
+
+            arr7[1][0] = new string[3];
+            arr7[1][1] = new string[4];
+            arr7[1][2] = new string[5];
+            arr7[1][3] = new string[6];
+
+            arr7[0][0][0] = "000";
+            arr7[0][0][1] = "001";
+            arr7[0][0][2] = "002";
+                            
+            arr7[0][1][0] = "010";
+            arr7[0][1][1] = "011";
+            arr7[0][1][2] = "012";
+            arr7[0][1][3] = "013";
+                            
+            arr7[0][2][0] = "020";
+            arr7[0][2][1] = "021";
+            arr7[0][2][2] = "022";
+            arr7[0][2][3] = "023";
+            arr7[0][2][4] = "024";
+                            
+
+            arr7[1][0][0] = "100";
+            arr7[1][0][1] = "101";
+            arr7[1][0][2] = "102";
+                             
+            arr7[1][1][0] = "110";
+            arr7[1][1][1] = "111";
+            arr7[1][1][2] = "112";
+            arr7[1][1][3] = "113";
+                             
+            arr7[1][2][0] = "120";
+            arr7[1][2][1] = "121";
+            arr7[1][2][2] = "122";
+            arr7[1][2][3] = "123";
+            arr7[1][2][4] = "124";
+                             
+            arr7[1][3][0] = "130";
+            arr7[1][3][1] = "131";
+            arr7[1][3][2] = "132";
+            arr7[1][3][3] = "133";
+            arr7[1][3][4] = "134";
+            arr7[1][3][5] = "135";
+
+            Console.WriteLine("arr7 : ");
+            Console.WriteLine("[");
+            for (int i = 0; i < arr7.GetLength(0); i++)
+            {
+                Console.WriteLine(" [");
+                for (int j = 0; j < arr7[i].GetLength(0); j++)
+                {
+                    Console.Write("  [");
+                    for (int k = 0; k < arr7[i][j].GetLength(0); k++)
+                    {
+                        Console.Write($"  {arr7[i][j][k]}  ");
+                    }
+                    Console.WriteLine("]");
+                }
+                Console.WriteLine(" ]");
+            }
+            Console.WriteLine("]");
 
 
 
         }
+
+        public static string Repeate(string str, int count)
+        {
+            return new StringBuilder(str.Length * count).Insert(0, str, count).ToString();
+        }
+
+        public static int CountDigits(int number)
+        {
+            return number.ToString().Length;
+        }
+
+
+
+
     }
 
 
